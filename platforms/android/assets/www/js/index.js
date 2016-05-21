a55e5dacc30a6878c5b628d1b5a5617bb9becc7e
avargas79 <info.sonaya@gmail.com>
2016-05-21 05:11:24 -0500

    nbjkhjk
    lklkl

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function mapa()
+{
+    
+    var latitud;
+    var longitud;
+    
+    var error = function(position)
+    {
+        alert("No se puede encontrar la posicion actual");        
+    }
+    
+    
+    var exito = function(position)
+    {
+        
+        latitud = position.coords.latitude;
+        longitud = position.coords.longitude;
+        
+        
+        map = new GMaps({
+            div:'#resultado',
+            lat: latitud,
+            lng: longitud,
+            zoom: 10,
+            
+            
+            
+        });
+        
+        
+        
+        map.addMarker({
+            lat: latitud,
+            lng: longitud,
+            title: 'posicion1',
+            infoWindow:{content: 'Esta es su posicion actual'}
+            
+        });
+    }
+    
+    
+    navigator.geolocation.getCurrentPosition(exito , error ,{timeout: 10000});
+}
+
+
+
+
+
+
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
